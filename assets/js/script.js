8f0d88f59f3a5c0d4a9dec176673cec1461ffd1e
yanagita1007 <1007yanagita@gmail.com>
2023-09-17 22:01:27 +0900

    cssを色々変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,9 +23,12 @@ const addMemoButton = document.querySelector('.add-memo');
 addMemoButton.addEventListener('click',function(){
     const newMemoInput = document.querySelector('.new-memo');
     const memoValue = newMemoInput.value;
-    console.log(memoValue);
+
     const newMemo = document.createElement('li');
     newMemo.textContent = newMemoInput.value;
+
+    // 新しいクラスを追加
+    newMemo.classList.add('memo-text');
 
     const memoList = document.querySelector('.memo-list');
     memoList.append(newMemo);
